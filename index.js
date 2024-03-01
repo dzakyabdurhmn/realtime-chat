83127e55e65a2b795ea40567061b215a565f6402
Dzaky Abdurahman <m.dzaky.abd123@gmail.com>
2024-03-02 00:00:13 +0700

    <feat>[connect server to client] #2

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-// 1. Packages
 import express from "express";
 import http from "http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
-import { Server as Socket } from "socket.io";
+import { Server } from "socket.io";
 
 // 2. Instances
 
 const app = express();
 const server = http.createServer(app);
-const io = new Socket(server);
+const io = new Server(server);
 
 // 3. Servering HTML file
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
 // 4. Define a connection event handler
 
+io.on("connection", (socket) => {
+  console.log("user connect to (Server) 🟢");
+  //   console.log(socket);
+});
+
 // 5. start the server
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port :${PORT}`));
--- readme.md
+++ readme.md
@@ -5,3 +5,10 @@ Penjelasan si kata gw
 
 dan apa itu komunikasi dua arah?
 - komunikasi dua arah adalah komunikasi yang memungkinkan pengiriman data dari client ke server dan sebaliknya.
+
+
+- io.on('connection', function(socket){...} adalah event yang terjadi ketika client terhubung ke server.
+
+<!-- io.on adalah metode yang digunakan untuk mendaftarkan pendengar peristiwa
+untuk berbagai peristiwa yang terjadi di sisi server. Objek io mewakili
+instance server Socket.IO utama. -->
